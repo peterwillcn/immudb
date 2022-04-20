8c4b5df3736e11fd2d4c4c63e6ab0fa38af93c32
Bartłomiej Święcki <bart@codenotary.com>
2022-04-20 16:13:00 +0200

    fix(sql): Add missing conversion functions to float
    Signed-off-by: Bartłomiej Święcki <bart@codenotary.com>

--- embedded/sql/catalog.go
+++ embedded/sql/catalog.go
@@ -424,6 +424,8 @@ func validMaxLenForType(maxLen int, sqlType SQLValueType) bool {
 		return maxLen <= 1
 	case IntegerType:
 		return maxLen == 0 || maxLen == 8
+	case Float64Type:
+		return maxLen == 0 || maxLen == 8
 	case TimestampType:
 		return maxLen == 0 || maxLen == 8
 	}
